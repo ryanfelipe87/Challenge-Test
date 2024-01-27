805d7c0a286401278cdcd003996a65c73165fbd5
Ryan <ryanfelipe.rr@gmail.com>
2024-01-27 17:18:57 -0300

    Method in Repository of the Filme

--- src/main/java/com/org/ecm/challengetest/repositories/FilmeRepository.java
+++ src/main/java/com/org/ecm/challengetest/repositories/FilmeRepository.java
@@ -1,9 +1,24 @@
 package com.org.ecm.challengetest.repositories;
 
+import com.org.ecm.challengetest.dtos.FilmeDto;
 import com.org.ecm.challengetest.models.Filme;
+import com.org.ecm.challengetest.models.Genero;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+
+    Optional<Filme> findByCodigo(String codigo);
+
+    void deleteByCodigo(String codigo);
+
+//    Filme findByNome(String nome);
+//
+//    List<FilmeDto> findByGeneros(Genero genero);
+//
+//    List<FilmeDto> buscarFilmesSemDisponibilidade();
 }
