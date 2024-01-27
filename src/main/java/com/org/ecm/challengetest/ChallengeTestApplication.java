6fa7a7f755f26f7fa61c63cc9e855f6aac3830b4
Ryan <ryanfelipe.rr@gmail.com>
2024-01-26 23:47:56 -0300

    Structure of the project

---
+++ src/main/java/com/org/ecm/challengetest/ChallengeTestApplication.java
@@ -0,0 +1,13 @@
+package com.org.ecm.challengetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/ecm/challengetest/ChallengeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.ecm.challengetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
