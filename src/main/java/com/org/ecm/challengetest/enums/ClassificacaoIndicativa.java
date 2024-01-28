eeda713359f8952e0f0c981ebc1ad87556309664
Ryan <ryanfelipe.rr@gmail.com>
2024-01-27 22:33:20 -0300

    EnumType for ClassificacaoIndicativa

---
+++ src/main/java/com/org/ecm/challengetest/enums/ClassificacaoIndicativa.java
@@ -0,0 +1,36 @@
+package com.org.ecm.challengetest.enums;
+
+public enum ClassificacaoIndicativa {
+
+    LIVRE("L", "Classificação Livre"),
+    DEZ("10", "Não recomendado para menores de 10 anos"),
+    DOZE("12", "Não recomendado para menores de 12 anos"),
+    CATORZE("14", "Não recomendado para menores de 14 anos"),
+    DEZESSEIS("16", "Não recomendado para menores de 16 anos"),
+    DEZOITO("18", "Não recomendado para menores de 18 anos");
+
+    private final String codigo;
+    private final String descricao;
+
+    ClassificacaoIndicativa(String codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static ClassificacaoIndicativa getByCodigo(String codigo) {
+        for (ClassificacaoIndicativa value : ClassificacaoIndicativa.values()) {
+            if (value.codigo.equals(codigo)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Código de classificação indicativa inválido: " + codigo);
+    }
+}
